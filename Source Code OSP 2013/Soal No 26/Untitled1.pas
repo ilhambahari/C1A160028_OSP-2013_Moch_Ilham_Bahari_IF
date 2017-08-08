82ac289b894064f71db111b880dc749b9af96e5c
ilhambahari <30597768+ilhambahari@users.noreply.github.com>
2017-08-08 23:05:03 +0700

    Update Untitled1.pas

--- Source Code OSP 2013/Soal No 26/Untitled1.pas
+++ Source Code OSP 2013/Soal No 26/Untitled1.pas
@@ -1,18 +1,24 @@
 uses crt;
 var
-N, hasil : integer;
+N,hasil : integer;
 procedure solve(x : integer);
+var
+hasildiv,hasilmod,jumlah : integer;
 begin
-if(x > 1) then
-begin
-hasil:=hasil+1;
-solve(x div 2 + x mod 2);
+  if(x > 1) then
+  begin
+    hasildiv:=x div 2;
+    hasilmod:=x mod 2;
+    jumlah:=hasildiv + hasilmod;
+    hasil:=hasil+1;
+    solve(hasildiv + hasilmod);
+    writeln('Solve(',hasildiv,' + ',hasilmod,') = ',jumlah,' ');
+  end;
 end;
-end;
 begin
-write('Masukkan N : '); readln(N);
-hasil:=0;
-solve(N);
-writeln(hasil);
+  write('Masukkan N : '); readln(N);
+  hasil:=0;
+  solve(N);
+  write('Hasil : ',hasil);
 readkey;
 end.
