4d0a7960a7e236a1c1dbd95b5b5301b2352767ec
ilhambahari <30597768+ilhambahari@users.noreply.github.com>
2017-08-10 02:00:29 +0700

    Update Untitled1.pas

--- Source Code OSP 2013/Soal No 32/Untitled1.pas
+++ Source Code OSP 2013/Soal No 32/Untitled1.pas
@@ -1,13 +1,16 @@
 uses crt;
 var
-i,j,k,a,b:integer;
+i,j,k,a,b,l:integer;
 procedure tulis(n,m:integer);
 begin
   for i:=1 to n do
     begin
      for j:=1 to (n div m) do
        for k:=1 to m do
-       writeln('*');
+         begin
+           l:=l+1;
+           writeln(l,' *');
+         end;
      for j:=1 to (n mod m) do
       writeln('-');
      end;
