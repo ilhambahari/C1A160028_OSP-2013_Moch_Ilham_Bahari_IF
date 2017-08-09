5889725e078453456d021038f991a7c26e3fe11a
ilhambahari <30597768+ilhambahari@users.noreply.github.com>
2017-08-10 02:03:00 +0700

    Update Untitled1.pas

--- Source Code OSP 2013/Soal No 36 dan 37/Untitled1.pas
+++ Source Code OSP 2013/Soal No 36 dan 37/Untitled1.pas
@@ -4,30 +4,50 @@ a : integer;
 function hap(x,t: integer): integer;
 begin
   if t = 1 then
-    hap := x mod 5
+    begin
+      hap := x mod 5;
+      writeln('hap:= ',x,' mod ',5,' =',hap,'');
+    end
   else
-    hap := 5*x;
+    begin
+      hap := 5*x;
+      writeln('hap:= ',5,'*',x,'  = ',hap,'');
     end;
- 
+    end;
 function hip(x,y: integer): integer;
 begin
   if x < y then
-    hip := hip(y,x)
+    begin
+      hip := hip(y,x);
+      writeln('hip:=hip(',y,',',x,')');
+    end
   else
-    hip := hap(x,1) + hap(y,2);
+    begin
+      hip := hap(x,1) + hap(y,2);
+      writeln('hip:=hap(',x,',',1,') + hap(',y,',',2,')');
+    end;
 end; 
  
 function hop(x,y,z: integer): integer;
 begin
   if y > z then
-    hop := hop(x,z,y)
+    begin
+      hop := hop(x,z,y);
+      writeln('hop:=hop(',x,',',z,',',y,')');
+    end
   else if x > y then
-    hop := hop(y,x,z)
+    begin
+      hop := hop(y,x,z);
+      writeln('hop:=hop(',y,',',x,',',z,')');
+      end
   else
-    hop := hip(x,y) + z;
+    begin
+      hop := hip(x,y) + z;
+      writeln('hop:=hip(',x,',',y,') + ',z,'');
+    end;
 end;
 begin
 a:=hop(18,3,1993);
-write(a);
+write('hasil : ',a);
 readkey;
 end.
