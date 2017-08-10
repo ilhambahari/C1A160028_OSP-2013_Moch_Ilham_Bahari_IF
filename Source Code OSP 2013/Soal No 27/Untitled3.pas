59f23566297e75afbd5f729741b928f1914fc6b7
ilhambahari <30597768+ilhambahari@users.noreply.github.com>
2017-08-10 21:38:09 +0700

    Add files via upload

---
+++ Source Code OSP 2013/Soal No 27/Untitled3.pas
@@ -0,0 +1,16 @@
+uses crt;
+var
+  kata,kalimat,base : string;
+  i : integer;
+begin
+  base := 'QWERTYUIOPLKJHGFDSAZXCVBNM';
+  kata := '';
+  kalimat := 's4yA-BuK4N+oRanG aLaY!?' ;
+for i:= length(kalimat) downto 1 do
+begin
+  if pos(kalimat[i], base) > 0 then
+  kata:= kata + kalimat[i];
+end;
+writeln(kata);
+readkey;
+end.
